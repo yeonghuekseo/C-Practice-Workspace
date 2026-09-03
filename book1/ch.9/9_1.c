3c72189f6c6239d21d74ab51c0108756aafbd6b9
yeonghuekseo <kipper0604@gmail.com>
2026-09-03 11:52:35 +0900

    .

--- book1/ch.9/9_1.c
+++ book1/ch.9/9_1.c
@@ -6,9 +6,9 @@ int main (void)
     double b;
     char c;
 
-    printf("int형 변수의 주소 : %u\n", &a);
-    printf("double형 변수의 주소 : %u\n", &b);
-    printf("char형 변수의 주소 : %u\n", &c);
+    printf("int형 변수의 주소 : %u\n", &a);     //int형 변수 선언
+    printf("double형 변수의 주소 : %u\n", &b);  //double형 변수 선언
+    printf("char형 변수의 주소 : %u\n", &c);    //char형 변수 선언
 
     return 0;
 }
